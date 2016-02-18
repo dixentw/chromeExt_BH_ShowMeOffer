9bf41d126d8ff7bb3304460bde0fd392acb28e75
Dixentw <dixentw@gmail.com>
2016-02-18 16:32:34 +0800

    draft input ui

--- writer/main.js
+++ writer/main.js
@@ -5,9 +5,9 @@ import ReactDOM from 'react-dom';
 
 import RaisedButton from 'material-ui/lib/raised-button';
 import Colors from 'material-ui/lib/styles/colors';
+import TextField from 'material-ui/lib/text-field';
 import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/lib/MuiThemeProvider';
-
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -21,13 +21,24 @@ const muiTheme = getMuiTheme({
 class WriteApp extends React.Component {
     render(){
         return (
-            <MuiThemeProvider muiTheme={muiTheme}>
+            <div>
+                <TextField floatingLabelText="* 公司名稱" />
+                <br/>
+                <TextField floatingLabelText="* 職缺名稱" />
+                <br/>
+                <TextField
+                    floatingLabelText="* 面試心得"
+                    multiLine={true}
+                />
+                <br/>
+                <TextField floatingLabelText="* 作者"/>
+                <br/>
                 <RaisedButton
-                    label="Super Secret Password"
+                    label="送出"
                     primary={true}
                     onTouchTap={this.handleTouchTap}
                 />
-            </MuiThemeProvider>
+            </div>
         );
     }
 }
